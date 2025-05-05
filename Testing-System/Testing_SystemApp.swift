8a6dede53a81968407b0bcc96ece3902903c7c35
Ihor Shevchuk <ihorshev@gmail.com>
2025-05-04 22:37:19 -0700

    Added empty iOS project

---
+++ Testing-System/Testing_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_SystemApp.swift
+//  Testing-System
+//
+//  Created by Ihor Shevchuk on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Testing_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
